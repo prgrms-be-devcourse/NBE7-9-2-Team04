0bd2b8736fef13fd170a354e5dc1a1a08233212e
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-13 12:07:40 +0900

    UserLoginRequestDto valid 추가

--- backend/src/main/java/com/backend/domain/user/dto/request/UserLoginRequestDto.java
+++ backend/src/main/java/com/backend/domain/user/dto/request/UserLoginRequestDto.java
@@ -1,9 +1,16 @@
 package com.backend.domain.user.dto.request;
 
 import com.backend.domain.user.entity.Users;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserLoginRequestDto(
+
+        @Email
+        @NotBlank
         String email,
+
+        @NotBlank
         String password
 ) {
     public UserLoginRequestDto(Users users){
