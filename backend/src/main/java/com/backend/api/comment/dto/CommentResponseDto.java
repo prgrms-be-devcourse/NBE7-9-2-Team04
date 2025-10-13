75e422c9e78e56af9f174f709a069c5ea36ebd7a
Labtory-82 <chnny9@gmail.com>
2025-10-13 15:55:38 +0900

    [feat] 댓글 엔티티, DTO 생성

--- backend/src/main/java/com/backend/api/comment/dto/CommentResponseDto.java
+++ backend/src/main/java/com/backend/api/comment/dto/CommentResponseDto.java
@@ -2,14 +2,22 @@ package com.backend.api.comment.dto;
 
 import java.time.LocalDateTime;
 import com.backend.domain.comment.entity.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CommentResponseDto(
+        @Schema(description = "댓글 ID", example = "1")
         Long id,
+        @Schema(description = "작성일", example = "2025-10-13T11:00:00")
         LocalDateTime createDate,
+        @Schema(description = "수정일", example = "2025-10-13T12:00:00")
         LocalDateTime modifyDate,
+        @Schema(description = "댓글 내용", example = "이것은 댓글입니다.")
         String content,
+        @Schema(description = "작성자 ID", example = "1")
         Long authorId,
-        String authorName,
+        @Schema(description = "작성자 닉네임", example = "user123")
+        String authorNickName,
+        @Schema(description = "게시글 ID", example = "1")
         Long postId
 ) {
     public CommentResponseDto(Comment comment) {
