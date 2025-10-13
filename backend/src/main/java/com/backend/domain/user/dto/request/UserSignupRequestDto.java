a0576498e9729f460fa1c7407baf53502ebcac9f
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-13 12:02:29 +0900

    feat: UserSignupRequestDto valid추가

--- backend/src/main/java/com/backend/domain/user/dto/request/UserSignupRequestDto.java
+++ backend/src/main/java/com/backend/domain/user/dto/request/UserSignupRequestDto.java
@@ -1,14 +1,28 @@
 package com.backend.domain.user.dto.request;
 
 import com.backend.domain.user.entity.Users;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record UserSignupRequestDto(
+        @Email
         String email,
+
+        @NotBlank
         String password,
+
+        @NotBlank
         String name,
+
+        @NotBlank
         String nickname,
+
+        @NotBlank
         Integer age,
+
+        @NotBlank
         String github,
+
         String image
 ) {
     public UserSignupRequestDto(Users users){
