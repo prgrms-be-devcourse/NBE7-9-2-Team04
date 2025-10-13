c30efee6c80c8374eda63cbb3a202f449f65732e
myoungjinseo <sih5673@gmail.com>
2025-10-13 11:34:13 +0900

    feat: 커스텀 예외 처리, valid 유효성 검사 처리, 그 외 모든 예외처리 설정

--- backend/src/main/java/com/backend/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/backend/global/exception/GlobalExceptionHandler.java
@@ -1,7 +1,43 @@
 package com.backend.global.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import com.backend.global.dto.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    // 커스텀 예외 처리
+    @ExceptionHandler(ErrorException.class)
+    protected ResponseEntity<ApiResponse<?>> handleCustomException(ErrorException e) {
+        log.error("ErrorException: {} - {}", e.getErrorCode().name(), e.getMessage(), e);
+        return new ResponseEntity<>(
+                ApiResponse.fail(e)
+                , e.getErrorCode().getHttpStatus());
+    }
+
+    // @Valid 유효성 검사 실패 시 발생하는 예외 처리
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+
+        String message = e.getBindingResult().getFieldError().getDefaultMessage();
+
+        log.warn("MethodArgumentNotValidException {}", e.getMessage());
+        return new ResponseEntity<>(
+                ApiResponse.fail(HttpStatus.BAD_REQUEST,message)
+                , HttpStatus.BAD_REQUEST);
+    }
+
+    // 그 외 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleAllException(final Exception e) {
+        log.error("handleAllException {}", e.getMessage(), e);
+        return new ResponseEntity<>(
+                ApiResponse.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
