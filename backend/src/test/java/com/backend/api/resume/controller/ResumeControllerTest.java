29062edad1f1945d083ccb5af26db4800bf71c11
myoungjinseo <sih5673@gmail.com>
2025-10-13 18:08:11 +0900

    test: 정상 테스트 및 실패 테스트 #12

--- backend/src/test/java/com/backend/api/resume/controller/ResumeControllerTest.java
+++ backend/src/test/java/com/backend/api/resume/controller/ResumeControllerTest.java
@@ -3,10 +3,9 @@ package com.backend.api.resume.controller;
 
 import com.backend.api.resume.dto.request.ResumeCreateRequest;
 import com.backend.domain.resume.repository.ResumeRepository;
-import com.backend.domain.user.entity.Users;
+import com.backend.domain.user.entity.User;
 import com.backend.domain.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.oas.annotations.media.Schema;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -42,16 +41,17 @@ class ResumeControllerTest {
 
     @BeforeAll
     void setUp() {
-        Users users = Users.builder()
+        User user = User.builder()
                 .email("test@naver.com")
                 .age(27)
                 .github("https://github.com/test")
                 .name("test")
-                .password("test")
+                .password("test1234")
                 .image(null)
-                .role(Users.Role.USER)
+                .role(User.Role.USER)
+                .nickname("testnick")
                 .build();
-        userRepository.save(users);
+        userRepository.save(user);
 
     }
 
@@ -75,7 +75,7 @@ class ResumeControllerTest {
             Long userId = 1L;
             // when
             ResultActions resultActions = mockMvc.perform(
-                    post("api/v1/users/resumes/%d".formatted(userId))
+                    post("/api/v1/users/resumes/%d".formatted(userId))
                             .accept(MediaType.APPLICATION_JSON)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(request))
@@ -85,7 +85,7 @@ class ResumeControllerTest {
                     .andExpect(handler().handlerType(ResumeController.class))
                     .andExpect(handler().methodName("createResume"))
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.status").value(200))
+                    .andExpect(jsonPath("$.status").value("OK"))
                     .andExpect(jsonPath("$.message").value("이력서가 생성되었습니다."))
                     .andExpect(jsonPath("$.data.userId").value(1))
                     .andExpect(jsonPath("$.data.content").value("이력서 내용입니다."))
@@ -112,7 +112,7 @@ class ResumeControllerTest {
             Long userId = 999L;
             // when
             ResultActions resultActions = mockMvc.perform(
-                    post("api/v1/users/resumes/%d".formatted(userId))
+                    post("/api/v1/users/resumes/%d".formatted(userId))
                             .accept(MediaType.APPLICATION_JSON)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(request))
@@ -123,7 +123,7 @@ class ResumeControllerTest {
                     .andExpect(handler().methodName("createResume"))
                     .andExpect(status().isNotFound())
                     .andExpect(jsonPath("$.status").value("NOT_FOUND"))
-                    .andExpect(jsonPath("$.message").value("해당 사용자가 존재하지 않습니다."))
+                    .andExpect(jsonPath("$.message").value("유저를 찾을 수 없습니다."))
                     .andDo(print());
         }
 
@@ -142,14 +142,14 @@ class ResumeControllerTest {
             Long userId = 1L;
             // when
             mockMvc.perform(
-                    post("api/v1/users/resumes/%d".formatted(userId))
+                    post("/api/v1/users/resumes/%d".formatted(userId))
                             .accept(MediaType.APPLICATION_JSON)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(request))
             );
 
             ResultActions resultActions = mockMvc.perform(
-                    post("api/v1/users/resumes/%d".formatted(userId))
+                    post("/api/v1/users/resumes/%d".formatted(userId))
                             .accept(MediaType.APPLICATION_JSON)
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(objectMapper.writeValueAsString(request))
@@ -160,7 +160,7 @@ class ResumeControllerTest {
                     .andExpect(handler().methodName("createResume"))
                     .andExpect(status().isBadRequest())
                     .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
-                    .andExpect(jsonPath("$.message").value("이미 이력서가 존재합니다."))
+                    .andExpect(jsonPath("$.message").value("이미 등록된 이력서가 있습니다."))
                     .andDo(print());
         }
     }
