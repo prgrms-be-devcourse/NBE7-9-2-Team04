2b1336a450e945bad1886602df68bb96b0b5a16c
myoungjinseo <sih5673@gmail.com>
2025-10-13 12:46:12 +0900

    feat: 이력서 레포지토리 구현

---
+++ backend/src/main/java/com/backend/domain/resume/repository/ResumeRepository.java
@@ -0,0 +1,7 @@
+package com.backend.domain.resume.repository;
+
+import com.backend.domain.resume.entity.Resume;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeRepository extends JpaRepository<Resume, Long> {
+}
