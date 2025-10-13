b8df3df9502e7d5cead660319322790b531664ec
myoungjinseo <sih5673@gmail.com>
2025-10-13 17:24:12 +0900

    test: 이력서 생성 api 테스트 코드

--- backend/src/test/java/com/backend/api/resume/controller/ResumeControllerTest.java
+++ backend/src/test/java/com/backend/api/resume/controller/ResumeControllerTest.java
@@ -1,28 +1,60 @@
 package com.backend.api.resume.controller;
 
 
+import com.backend.api.resume.dto.request.ResumeCreateRequest;
 import com.backend.domain.resume.repository.ResumeRepository;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import com.backend.domain.user.entity.Users;
+import com.backend.domain.user.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.MediaType;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 @Transactional
 @ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ResumeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private ResumeRepository resumeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeAll
+    void setUp() {
+        Users users = Users.builder()
+                .email("test@naver.com")
+                .age(27)
+                .github("https://github.com/test")
+                .name("test")
+                .password("test")
+                .image(null)
+                .role(Users.Role.USER)
+                .build();
+        userRepository.save(users);
+
+    }
+
 
     @Nested
     @DisplayName("이력서 생성 API")
@@ -32,10 +64,104 @@ class ResumeControllerTest {
         @DisplayName("정상 작동")
         void success() throws Exception {
             // given
-
+            ResumeCreateRequest request = new ResumeCreateRequest(
+                    "이력서 내용입니다.",
+                    "Java, Spring Boot",
+                    "대외 활동 내용입니다.",
+                    "없음",
+                    "경력 사항 내용입니다.",
+                    "http://portfolio.example.com"
+            );
+            Long userId = 1L;
             // when
-
+            ResultActions resultActions = mockMvc.perform(
+                    post("api/v1/users/resumes/%d".formatted(userId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
             // then
+            resultActions
+                    .andExpect(handler().handlerType(ResumeController.class))
+                    .andExpect(handler().methodName("createResume"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status").value(200))
+                    .andExpect(jsonPath("$.message").value("이력서가 생성되었습니다."))
+                    .andExpect(jsonPath("$.data.userId").value(1))
+                    .andExpect(jsonPath("$.data.content").value("이력서 내용입니다."))
+                    .andExpect(jsonPath("$.data.skill").value("Java, Spring Boot"))
+                    .andExpect(jsonPath("$.data.activity").value("대외 활동 내용입니다."))
+                    .andExpect(jsonPath("$.data.certification").value("없음"))
+                    .andExpect(jsonPath("$.data.career").value("경력 사항 내용입니다."))
+                    .andExpect(jsonPath("$.data.portfolioUrl").value("http://portfolio.example.com"))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("userId가 존재하지 않을 떄")
+        void fail1() throws Exception {
+            // given
+            ResumeCreateRequest request = new ResumeCreateRequest(
+                    "이력서 내용입니다.",
+                    "Java, Spring Boot",
+                    "대외 활동 내용입니다.",
+                    "없음",
+                    "경력 사항 내용입니다.",
+                    "http://portfolio.example.com"
+            );
+            Long userId = 999L;
+            // when
+            ResultActions resultActions = mockMvc.perform(
+                    post("api/v1/users/resumes/%d".formatted(userId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
+            // then
+            resultActions
+                    .andExpect(handler().handlerType(ResumeController.class))
+                    .andExpect(handler().methodName("createResume"))
+                    .andExpect(status().isNotFound())
+                    .andExpect(jsonPath("$.status").value("NOT_FOUND"))
+                    .andExpect(jsonPath("$.message").value("해당 사용자가 존재하지 않습니다."))
+                    .andDo(print());
+        }
+
+        @Test
+        @DisplayName("이미 이력서가 존재할 때")
+        void fail2() throws Exception {
+            // given
+            ResumeCreateRequest request = new ResumeCreateRequest(
+                    "이력서 내용입니다.",
+                    "Java, Spring Boot",
+                    "대외 활동 내용입니다.",
+                    "없음",
+                    "경력 사항 내용입니다.",
+                    "http://portfolio.example.com"
+            );
+            Long userId = 1L;
+            // when
+            mockMvc.perform(
+                    post("api/v1/users/resumes/%d".formatted(userId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
+
+            ResultActions resultActions = mockMvc.perform(
+                    post("api/v1/users/resumes/%d".formatted(userId))
+                            .accept(MediaType.APPLICATION_JSON)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+            );
+            // then
+            resultActions
+                    .andExpect(handler().handlerType(ResumeController.class))
+                    .andExpect(handler().methodName("createResume"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                    .andExpect(jsonPath("$.message").value("이미 이력서가 존재합니다."))
+                    .andDo(print());
         }
     }
 
