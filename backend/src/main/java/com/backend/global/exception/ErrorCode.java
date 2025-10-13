8b3672a864295788740f861bfaab2503de203d40
myoungjinseo <sih5673@gmail.com>
2025-10-13 18:08:58 +0900

    feat: 예외처리 추가 #12

--- backend/src/main/java/com/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/ErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     UNKNOWN_ERROR(HttpStatus.UNAUTHORIZED, "알 수 없는 에러"),
-    NOT_FOUND_NICKNAME(HttpStatus.NOT_FOUND,"작성자를 찾을 수 없습니다.");
-
+    NOT_FOUND_NICKNAME(HttpStatus.NOT_FOUND,"작성자를 찾을 수 없습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND,"유저를 찾을 수 없습니다."),
+    // resume
+    DUPLICATE_RESUME(HttpStatus.BAD_REQUEST, "이미 등록된 이력서가 있습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
