0981612f3643dce062d3b83fbd823b9e1a77587d
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-14 12:26:49 +0900

    feat: 토큰 만료시간 설정

--- backend/src/main/java/com/backend/global/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/backend/global/jwt/JwtTokenProvider.java
@@ -1,10 +1,28 @@
 package com.backend.global.jwt;
 
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
 
+    @Value("${secretPattern}")
+    private String secretPattern;
 
+    @Value("${jwt.access-token.expire-time}")
+    private long ACCESS_TOKEN_EXPIRE_TIME;
+
+    @Value("${jwt.refresh-token.expire-time}")
+    private long REFRESH_TOKEN_EXPIRE_TIME;
+
+    private JwtParser jwtParser;
+
+    @PostConstruct
+    void init() {
+        this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretPattern));
+    }
 }
