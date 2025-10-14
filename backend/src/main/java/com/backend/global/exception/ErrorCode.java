4b15a9d27289c57aef54f237a8facb1bb3b2198c
myoungjinseo <sih5673@gmail.com>
2025-10-14 12:10:52 +0900

    feat: 예외처리 추가 #36

--- backend/src/main/java/com/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/ErrorCode.java
@@ -19,7 +19,9 @@ public enum ErrorCode {
 
 
     // resume
-    DUPLICATE_RESUME(HttpStatus.BAD_REQUEST, "이미 등록된 이력서가 있습니다.");
+    DUPLICATE_RESUME(HttpStatus.BAD_REQUEST, "이미 등록된 이력서가 있습니다."),
+    NOT_FOUND_RESUME(HttpStatus.NOT_FOUND, "이력서를 찾을 수 없습니다."),
+    INVALID_USER(HttpStatus.FORBIDDEN, "이력서 수정 권한이 없습니다.");
 
 
     private final HttpStatus httpStatus;
