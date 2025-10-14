2a181e5033016834709f369af681e3f07dc9b310
Labtory-82 <chnny9@gmail.com>
2025-10-14 09:35:33 +0900

    feat: 빌더 사용을 위한 어노테이션 추가, 수동 생성자 AllArgsConstructor로 대체, update 메서드 이름 변경#33

--- backend/src/main/java/com/backend/domain/comment/entity/Comment.java
+++ backend/src/main/java/com/backend/domain/comment/entity/Comment.java
@@ -1,34 +1,27 @@
 package com.backend.domain.comment.entity;
 
-import com.backend.domain.user.entity.Users;
+import com.backend.domain.post.entity.Post;
+import com.backend.domain.user.entity.User;
 import com.backend.global.entity.BaseEntity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@NoArgsConstructor
-@Getter
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class Comment extends BaseEntity {
 
     private String content;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "postId")
     private Post post;
     @ManyToOne(fetch = FetchType.LAZY)
-    private Users author;
+    private User author;
 
-    public Comment(Users author, String content, Post post) {
-        this.author = author;
-        this.content = content;
-        this.post = post;
-    }
-
-    public void update(String content) {
+    public void updateContent(String content) {
         this.content = content;
     }
 
