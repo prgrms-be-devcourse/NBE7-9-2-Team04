0fa746121ddb8e655d6e2e21b24080f007b1890e
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-14 12:37:25 +0900

    refactor: role 분리

---
+++ backend/src/main/java/com/backend/domain/user/entity/Role.java
@@ -0,0 +1,8 @@
+package com.backend.domain.user.entity;
+
+import lombok.Getter;
+
+@Getter
+public enum Role {
+    USER, ADMIN
+}
--- backend/src/main/java/com/backend/global/jwt/JwtTokenProvider.java
+++ backend/src/main/java/com/backend/global/jwt/JwtTokenProvider.java
@@ -1,13 +1,19 @@
 package com.backend.global.jwt;
 
-import io.jsonwebtoken.JwtParser;
+import com.backend.domain.user.entity.Role;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtTokenProvider {
 
     @Value("${custom.jwt.secretPattern}")
@@ -21,8 +27,15 @@ public class JwtTokenProvider {
 
     private Key key;
 
+    //
     @PostConstruct
-    void init() {
+    public void init() {
+        //디코딩한 바이트 시퀀스 사용
         this.key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretPattern));
     }
+
+    //AccessToken 생성 (Role 포함)
+    public String generateAccessToken(String email, Role role) {}
+
+
 }
