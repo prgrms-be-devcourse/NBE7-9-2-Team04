99b5e1bca4d11a9ead05b08886ab229e63ce20e3
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-15 16:50:28 +0900

    feat: t: JWT 관련 테스트 코드 작성

--- backend/src/main/java/com/backend/api/user/controller/UserController.java
+++ backend/src/main/java/com/backend/api/user/controller/UserController.java
@@ -75,8 +75,9 @@ public class UserController {
         TokenResponse newTokens = userService.createAccessTokenFromRefresh(refreshToken);
 
         rq.setCookie("accessToken", newTokens.accessToken(), (int) (jwtTokenProvider.getAccessTokenExpireTime()));
+        rq.setCookie("refreshToken", newTokens.refreshToken(), (int) (jwtTokenProvider.getRefreshTokenExpireTime()));
 
-        return ApiResponse.ok("새로운 토큰이 발급되었습니다",  null);
+        return ApiResponse.ok("새로운 토큰이 발급되었습니다.",  null);
     }
 
 }
