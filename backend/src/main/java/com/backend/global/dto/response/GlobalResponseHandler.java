89f92ab57d03fc867bcf5aacdba4f1a5565f175a
myoungjinseo <sih5673@gmail.com>
2025-10-15 11:41:47 +0900

    feat: 커스텀 예외처리가 안 되어있을 경우

--- backend/src/main/java/com/backend/global/dto/response/GlobalResponseHandler.java
+++ backend/src/main/java/com/backend/global/dto/response/GlobalResponseHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
@@ -24,6 +25,11 @@ public class GlobalResponseHandler implements ResponseBodyAdvice<Object> {
                                   Class<? extends HttpMessageConverter<?>> selectedConverterType,
                                   ServerHttpRequest request,
                                   ServerHttpResponse response) {
+
+        // 혹시 모를 ErrorResponse 처리를 위해 -> 커스텀 예외처리가 아닐 경우
+        if (body instanceof ErrorResponse errorResponse) {
+            body = errorResponse.getBody();
+        }
 
         if (body instanceof ApiResponse apiResponse) {
             response.setStatusCode(apiResponse.status());
