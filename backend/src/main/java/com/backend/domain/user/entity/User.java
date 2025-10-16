ddb5ec66816477a3131312651173b5923f31b693
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-16 11:53:42 +0900

    fix: ususer resume 매핑

--- backend/src/main/java/com/backend/domain/user/entity/User.java
+++ backend/src/main/java/com/backend/domain/user/entity/User.java
@@ -1,12 +1,15 @@
 package com.backend.domain.user.entity;
 
+import com.backend.domain.resume.entity.Resume;
 import com.backend.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Entity
 @Builder
@@ -39,24 +42,8 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private Role role;
 
-    public User(String email,
-                String password,
-                String name,
-                String nickname,
-                int age,
-                String github,
-                String image,
-                Role role)
-    {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.nickname = nickname;
-        this.age = age;
-        this.github = github;
-        this.image = image;
-        this.role = role;
-    }
+    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Resume resume;
 
 
 }
