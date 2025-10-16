8ce8003547c9b2fc2c489f3152c7dcc192bb702a
김규섭 <ryan12256@naver.com>
2025-10-16 09:48:35 +0900

    test : 게시글 생성 API DTO

--- backend/src/main/java/com/backend/api/post/dto/response/PostResponse.java
+++ backend/src/main/java/com/backend/api/post/dto/response/PostResponse.java
@@ -3,26 +3,42 @@ package com.backend.api.post.dto.response;
 import com.backend.domain.post.entity.PinStatus;
 import com.backend.domain.post.entity.Post;
 import com.backend.domain.post.entity.PostStatus;
-
 import com.backend.global.exception.ErrorCode;
 import com.backend.global.exception.ErrorException;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 // 게시글 응답
-public record PostResponse(
-        Long postId,
-        String title,
-        String content,
-        LocalDateTime deadline,
-        LocalDateTime createDate,
-        LocalDateTime modifyDate,
-        PostStatus status,
-        PinStatus pinStatus,
-        String nickName // 작성자의 닉네임
-) {
+    public record PostResponse(
+            @Schema(description = "게시글 ID", example = "1")
+            Long postId,
+
+            @Schema(description = "제목", example = "첫번째 게시물")
+            String title,
+
+            @Schema(description = "내용", example = "함께 팀 프로젝트를 진행할 백엔드 개발자 구합니다.")
+            String content,
+
+            @Schema(description = "마감일", example = "2025-10-25T17:00:00")
+            LocalDateTime deadline,
+
+            @Schema(description = "작성일", example = "2025-10-18T10:30:00")
+            LocalDateTime createDate,
+
+            @Schema(description = "수정일", example = "2025-10-18T10:50:00")
+            LocalDateTime modifyDate,
+
+            @Schema(description = "진행 상태", example = "IN_PROGRESS", allowableValues = {"ING", "CLOSED"})
+            PostStatus status,
+
+            @Schema(description = "상단 고정 여부", example = "UNPINNED", allowableValues = {"PINNED", "NOT_PINNED"})
+            PinStatus pinStatus,
+
+            @Schema(description = "작성자의 닉네임", example = "개발자A")
+            String nickName // 작성자의 닉네임
+    ) {
 
     public static PostResponse from(Post post) {
 
