49fda9fc130a421f2377fb41d52ce732bafcc03c
김규섭 <ryan12256@naver.com>
2025-10-20 11:38:17 +0900

    refractor : 최신 main 반영 및 리팩토링

--- backend/src/main/java/com/backend/api/user/controller/UserMyPageController.java
+++ backend/src/main/java/com/backend/api/user/controller/UserMyPageController.java
@@ -1,5 +1,6 @@
 package com.backend.api.user.controller;
 
+import com.backend.api.post.dto.response.PostResponse;
 import com.backend.api.post.service.PostService;
 import com.backend.api.user.dto.response.UserMyPageResponse;
 import com.backend.api.user.service.UserMyPageService;
@@ -11,11 +12,13 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/users")
-@Tag(name ="Users", description = "마이페이지 관련 API")
+@Tag(name = "Users", description = "마이페이지 관련 API")
 public class UserMyPageController {
 
     private final UserService userService;
@@ -37,11 +40,14 @@ public class UserMyPageController {
 
         Long userId = rq.getUser().getId();
         UserMyPageResponse response = userMyPageService.modifyUser(userId, modify);
-        return ApiResponse.ok("개인 정보 수정이 완료되었습니다.",response);
+        return ApiResponse.ok("개인 정보 수정이 완료되었습니다.", response);
+
+    }
+
     @GetMapping("/{userId}")
     @Operation(summary = "마이페이지 상세 정보 조회")
     public ApiResponse<UserMyPageResponse> detailInformation(@PathVariable Long userId) {
-        UserMyPageResponse response = userService.getInformation(userId);
+        UserMyPageResponse response = userMyPageService.getInformation(userId);
         return ApiResponse.ok("사용자 상세 정보 조회를 완료했습니다.", response);
     }
 
