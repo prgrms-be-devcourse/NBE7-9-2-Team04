fd5af9d691aef957eec3ec4984263e66ac4de173
larama-C <ccbj0825@naver.com>
2025-10-20 21:51:15 +0900

    feat: 질문 카테고리 관련 에러코드 추가

--- backend/src/main/java/com/backend/global/exception/ErrorCode.java
+++ backend/src/main/java/com/backend/global/exception/ErrorCode.java
@@ -27,6 +27,10 @@ public enum ErrorCode {
     INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 Refresh Token입니다."),
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Refresh Token을 찾을 수 없습니다."),
 
+    //Category
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 카테고리입니다."),
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "유효하지 않은 카테고리입니다."),
+
 
     // Post & Comment
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 게시글입니다."),
