664045dd3889b1739ecdece76b0f0123b3746084
larama-C <ccbj0825@naver.com>
2025-10-21 23:11:46 +0900

    feat: 회원가입 시 이메일 인증 로직 구현 VerificationCode 인증 코드 전송/검증 로직 추가

--- backend/src/main/java/com/backend/domain/user/repository/VerificationCodeRepository.java
+++ backend/src/main/java/com/backend/domain/user/repository/VerificationCodeRepository.java
@@ -1,4 +1,10 @@
 package com.backend.domain.user.repository;
 
-public interface VerificationCodeRepository {
+import com.backend.domain.user.entity.VerificationCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+    Optional<VerificationCode> findByEmail(String email);
 }
