2ef1785542d6aedcd20384fb29773bf3071d8455
larama-C <ccbj0825@naver.com>
2025-10-21 23:09:32 +0900

    feat: 이메일 인증 VerificationCode 엔티티 및 레포지토리 추가

---
+++ backend/src/main/java/com/backend/domain/user/entity/VerificationCode.java
@@ -0,0 +1,4 @@
+package com.backend.domain.user.entity;
+
+public class verificationCode {
+}
---
+++ backend/src/main/java/com/backend/domain/user/repository/VerificationCodeRepository.java
@@ -0,0 +1,4 @@
+package com.backend.domain.user.repository;
+
+public interface VerificationCodeRepository {
+}
