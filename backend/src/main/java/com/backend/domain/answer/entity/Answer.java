d0a3d51c2fbcd211f629014c25a3220f8ad52476
Labtory-82 <chnny9@gmail.com>
2025-10-22 23:37:33 +0900

    refactor: JsonIgnore 추가#157

--- backend/src/main/java/com/backend/domain/answer/entity/Answer.java
+++ backend/src/main/java/com/backend/domain/answer/entity/Answer.java
@@ -3,6 +3,7 @@ package com.backend.domain.answer.entity;
 import com.backend.domain.question.entity.Question;
 import com.backend.domain.user.entity.User;
 import com.backend.global.entity.BaseEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -28,6 +29,7 @@ public class Answer extends BaseEntity {
     private User author;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
     @JoinColumn(nullable = false)
     private Question question;
 
