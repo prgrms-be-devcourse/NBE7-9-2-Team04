0aa0d29ea16a65a4b4a290ff647372c5e3bfe804
Labtory-82 <chnny9@gmail.com>
2025-10-22 18:38:08 +0900

    feat: 페이징 응답 dto에 전체 페이지 수, 전체 엔티티 수, 페이지 사이즈 필드 추가#148

--- backend/src/main/java/com/backend/api/post/dto/response/PostPageResponse.java
+++ backend/src/main/java/com/backend/api/post/dto/response/PostPageResponse.java
@@ -12,13 +12,19 @@ public record PostPageResponse<T>(
         @Schema(description = "현재 페이지 번호", example = "3")
         int currentPage,
         @Schema(description = "전체 페이지 수", example = "10")
-        int totalPages
+        int totalPages,
+        @Schema(description = "전체 게시글 수", example = "95")
+        int totalCount,
+        @Schema(description = "페이지당 게시글 수", example = "10")
+        int pageSize
 ) {
     public static <T> PostPageResponse<T> from(Page<Post> page, List<T> posts) {
         return new PostPageResponse<>(
                 posts,
                 page.getNumber() + 1,
-                page.getTotalPages()
+                page.getTotalPages(),
+                (int) page.getTotalElements(),
+                page.getSize()
         );
     }
 }
--- backend/src/main/java/com/backend/api/user/dto/response/UserPageResponse.java
+++ backend/src/main/java/com/backend/api/user/dto/response/UserPageResponse.java
@@ -12,13 +12,19 @@ public record UserPageResponse<T> (
         @Schema(description = "현재 페이지 번호", example = "3")
         int currentPage,
         @Schema(description = "전체 페이지 수", example = "10")
-        int totalPages
+        int totalPages,
+        @Schema(description = "전체 유저 수", example = "95")
+        int totalCount,
+        @Schema(description = "페이지당 유저 수", example = "10")
+        int pageSize
 ) {
     public static <T> UserPageResponse<T> from(Page<User> page, List<T> users) {
         return new UserPageResponse<>(
                 users,
                 page.getNumber() + 1,
-                page.getTotalPages()
+                page.getTotalPages(),
+                (int) page.getTotalElements(),
+                page.getSize()
         );
     }
 }
