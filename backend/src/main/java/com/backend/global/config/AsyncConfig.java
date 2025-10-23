6f1ce75341937d2812659f98828c195a8a42d93a
myoungjinseo <sih5673@gmail.com>
2025-10-24 00:06:43 +0900

    feat: 비동기 설정 #139

---
+++ backend/src/main/java/com/backend/global/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.backend.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "feedbackExecutor")
+    public Executor feedbackExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("feedback-");
+        executor.initialize();
+        return executor;
+    }
+
+
+}
