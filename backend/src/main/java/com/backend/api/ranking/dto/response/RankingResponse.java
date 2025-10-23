099687bc10be7a2cf72e082296a721b14073d920
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-24 04:46:10 +0900

    feat: ranking controller dto

--- backend/src/main/java/com/backend/api/ranking/dto/response/RankingResponse.java
+++ backend/src/main/java/com/backend/api/ranking/dto/response/RankingResponse.java
@@ -1,4 +1,33 @@
 package com.backend.api.ranking.dto.response;
 
-public class RankingResponse {
+import com.backend.domain.ranking.entity.Ranking;
+import com.backend.domain.ranking.entity.Tier;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record RankingResponse (
+    @Schema(description = "사용자 ID", example = "12")
+    Long userId,
+
+    @Schema(description = "닉네임", example = "김개발")
+    String nickName,
+
+    @Schema(description = "총 점수", example = "500")
+    int totalScore,
+
+    @Schema(description = "티어", example = "GOLD")
+    Tier tier,
+
+    @Schema(description = "순위", example = "15")
+    int rank
+)
+{
+    public static RankingResponse from(Ranking ranking) {
+        return new RankingResponse(
+                ranking.getUser().getId(),
+                ranking.getUser().getNickname(),
+                ranking.getTotalScore(),
+                ranking.getTier(),
+                ranking.getRank()
+        );
+    }
 }
