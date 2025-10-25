dff424ef4b865ef492771b5275bfd81d7dc87c17
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-10-25 20:17:22 +0900

    feat: accessToken 만료시 Refresh api 호출

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -12,7 +12,30 @@ export async function fetchApi(url: string, options?: RequestInit) {
   const fullUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`;
   console.log("fetch url:", fullUrl, options);
 
-  const res = await fetch(fullUrl, options);
+  let res = await fetch(fullUrl, options);
+
+  if(res.status ==401){
+    console.warn("AccessToken 만료. Refresh 실행");
+  }
+
+  const refreshRes = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/users/refresh`,
+    {
+      method: "POST",
+      credentials: "include",
+    }
+  );
+
+  if (refreshRes.ok) {
+    console.log("AccessToken Refresh 성공");
+    // 새 토큰이 쿠키로 설정되었으므로 다시 요청
+    res = await fetch(fullUrl, options);
+  } else {
+    console.error("Refresh 실패. 로그아웃 진행");
+    window.location.href = "/auth";
+    return;
+  }
+
   const apiResponse = await res.json();
 
   if (!res.ok) {
@@ -22,15 +45,3 @@ export async function fetchApi(url: string, options?: RequestInit) {
   // ApiResponse<T> 구조 그대로 반환
   return apiResponse;
 }
-
-// console.log("fetch url:", `${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options);
-
-// return fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}${url}`, options).then(
-//   async (res) => {
-//     if (!res.ok) {
-//       const apiResponse = await res.json();
-//       throw new Error(apiResponse.message || "요청 실패");
-//     }
-//     return res.json();
-//   }
-// );
