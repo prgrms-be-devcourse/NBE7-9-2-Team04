e10304e995befcd7c6c069745ab4294ab5722487
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-04 16:49:40 +0900

    feat: rankingtest 내 랭킹 조회 성공

--- backend/src/test/java/com/backend/api/ranking/controller/RankingControllerTest.java
+++ backend/src/test/java/com/backend/api/ranking/controller/RankingControllerTest.java
@@ -1,15 +1,11 @@
 package com.backend.api.ranking.controller;
 
-import com.backend.api.subscription.controller.SubscriptionController;
-import com.backend.domain.question.repository.QuestionRepository;
+import com.backend.domain.ranking.entity.Ranking;
+import com.backend.domain.ranking.entity.Tier;
 import com.backend.domain.ranking.repository.RankingRepository;
-import com.backend.domain.subscription.entity.Subscription;
-import com.backend.domain.subscription.entity.SubscriptionType;
-import com.backend.domain.subscription.repository.SubscriptionRepository;
 import com.backend.domain.user.entity.Role;
 import com.backend.domain.user.entity.User;
 import com.backend.domain.user.repository.UserRepository;
-import com.backend.domain.userQuestion.repository.UserQuestionRepository;
 import com.backend.global.Rq.Rq;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -25,14 +21,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -46,44 +38,37 @@ public class RankingControllerTest {
     private RankingRepository rankingRepository;
 
     @Autowired
-    private UserQuestionRepository userQuestionRepository;
-
-    @Autowired
-    private QuestionRepository questionRepository;
-
-    @Autowired
     private UserRepository userRepository;
 
     @MockBean
     private Rq rq;
 
-    private User testUser1;
-    private User testUser2;
+    private User testUser;
+    private Ranking testRanking;
 
     @BeforeEach
     void setUp(){
-        testUser1 = userRepository.save(
+        testUser = userRepository.save(
                 User.builder()
-                        .email("testuser1@test.com")
+                        .email("testuser@test.com")
                         .password("user1234!")
-                        .name("유저1")
-                        .nickname("user1")
+                        .name("유저")
+                        .nickname("user")
                         .age(25)
-                        .github("github.com/user1")
+                        .github("github.com/user")
                         .role(Role.USER)
                         .build());
 
+        testRanking = rankingRepository.save(
+                Ranking.builder()
+                        .user(testUser)
+                        .totalScore(100)
+                        .tier(Tier.UNRATED)
+                        .rankValue(3)
+                        .build()
+        );
 
-        testUser2 = userRepository.save(
-                User.builder()
-                        .email("testuser2@test.com")
-                        .password("user1234!")
-                        .name("유저2")
-                        .nickname("user2")
-                        .age(25)
-                        .github("github.com/user2")
-                        .role(Role.USER)
-                        .build());
+        when(rq.getUser()).thenReturn(testUser);
 
     }
 
@@ -95,8 +80,6 @@ public class RankingControllerTest {
         @DisplayName("정상 작동")
         void success() throws Exception{
 
-            //given
-            when(rq.getUser()).thenReturn(testUser1);
 
             //when
             ResultActions resultActions = mockMvc.perform(
@@ -112,14 +95,8 @@ public class RankingControllerTest {
                     .andExpect(handler().methodName("getRankings"))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.status").value("OK"))
-                    .andExpect(jsonPath("$.message").value("구독 정보를 불러왔습니다."))
-                    .andExpect(jsonPath("$.data.userId").value(testUser1.getId()))
-                    .andExpect(jsonPath("$.data.subscriptionName").value("PREMIUM"))
-                    .andExpect(jsonPath("$.data.subscriptionType").value("PREMIUM"))
-                    .andExpect(jsonPath("$.data.isActive").value(true))
-                    .andExpect(jsonPath("$.data.price").value(9900))
-                    .andExpect(jsonPath("$.data.customerKey").value("customerKey123"))
-                    .andExpect(jsonPath("$.data.billingKey").value("billingKey123"))
+                    .andExpect(jsonPath("$.message").value("전체 랭킹 조회를 성공했습니다."))
+                    .andExpect(jsonPath("$.data.userId").value(testUser.getId()))
                     .andDo(print());
 
         }
@@ -165,14 +142,12 @@ public class RankingControllerTest {
 
     @Nested
     @DisplayName("내 랭킹 조회 API")
-    class t2{
+    class t2 {
 
         @Test
         @DisplayName("정상 작동")
-        void success() throws Exception{
+        void success() throws Exception {
 
-            //given
-            when(rq.getUser()).thenReturn(testUser1);
 
             //when
             ResultActions resultActions = mockMvc.perform(
@@ -188,21 +163,18 @@ public class RankingControllerTest {
                     .andExpect(handler().methodName("getMyRankingOnly"))
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.status").value("OK"))
-                    .andExpect(jsonPath("$.message").value("구독 정보를 불러왔습니다."))
-                    .andExpect(jsonPath("$.data.userId").value(testUser1.getId()))
-                    .andExpect(jsonPath("$.data.subscriptionName").value("PREMIUM"))
-                    .andExpect(jsonPath("$.data.subscriptionType").value("PREMIUM"))
-                    .andExpect(jsonPath("$.data.isActive").value(true))
-                    .andExpect(jsonPath("$.data.price").value(9900))
-                    .andExpect(jsonPath("$.data.customerKey").value("customerKey123"))
-                    .andExpect(jsonPath("$.data.billingKey").value("billingKey123"))
+                    .andExpect(jsonPath("$.message").value("내 랭킹 조회를 성공했습니다."))
+                    .andExpect(jsonPath("$.data.userId").value(testUser.getId()))
+                    .andExpect(jsonPath("$.data.nickName").value("user"))
+                    .andExpect(jsonPath("$.data.totalScore").value(100))
+                    .andExpect(jsonPath("$.data.currentTier").value("UNRATED"))
                     .andDo(print());
 
         }
 
         @Test
         @DisplayName("내 랭킹 조회 실패 = 유저의 랭킹이 존재하지 않을 때")
-        void fail() throws Exception{
+        void fail() throws Exception {
 
             //given
             User newUser = userRepository.save(
@@ -233,8 +205,9 @@ public class RankingControllerTest {
                     .andExpect(handler().methodName("getMyRankingOnly"))
                     .andExpect(status().isNotFound())
                     .andExpect(jsonPath("$.status").value("NOT_FOUND"))
-                    .andExpect(jsonPath("$.message").value("해당 고객의 구독 정보를 찾을 수 없습니다."))
+                    .andExpect(jsonPath("$.message").value("해당 사용자의 랭킹 정보를 찾을 수 없습니다."))
                     .andDo(print());
 
         }
+    }
 }
