625b2e3ffc6cc0e8ffeff00cc2b0f805bcc7873b
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-07 01:19:44 +0900

    feat: RedisConfig 설정

--- backend/src/main/java/com/backend/domain/user/entity/RefreshToken.java
+++ backend/src/main/java/com/backend/domain/user/entity/RefreshToken.java
@@ -1,4 +1,20 @@
 package com.backend.domain.user.entity;
 
-public class RefreshToken {
+import com.backend.global.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Builder
+@RedisHash(value = "refreshToken", timeToLive = 604800) //7일
+public class RefreshToken extends BaseEntity {
+
+    private String refreshToken;
 }
--- backend/src/main/java/com/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/backend/global/config/RedisConfig.java
@@ -1,7 +1,44 @@
 package com.backend.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    //Redis와 연결을 위한 Connection 생성
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        final RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(host,port);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    //Redis 데이터 처리를 위한 템플릿
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+
+        //key:value 직렬화
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+
+        //hash 사용할 경우 직렬화
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new StringRedisSerializer());
+
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
 }
