8e942d0bab6643d384546b9e6885095dfcd2fb1d
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-07 01:29:00 +0900

    feat: RedisConfig 설정

--- backend/src/main/java/com/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/backend/global/config/RedisConfig.java
@@ -7,6 +7,8 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -25,18 +27,22 @@ public class RedisConfig {
         return new LettuceConnectionFactory(configuration);
     }
 
-    //Redis 데이터 처리를 위한 템플릿
+    //문자열만 사용할 때(refreshToken)
+    @Bean
+    public StringRedisTemplate stringRedisTemplate() {
+        return new StringRedisTemplate(redisConnectionFactory());
+    }
+
+    //Json 직렬화 필요할 때(랭킹, 복잡한 객체 저장 등)
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
 
-        //key:value 직렬화
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
-        //hash 사용할 경우 직렬화
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         template.setConnectionFactory(redisConnectionFactory());
         return template;
