3eff48731d59bce980f7dd5a0214df9dcb90ddd7
kimeunkyoungg <127072393+kimeunkyoungg@users.noreply.github.com>
2025-11-07 01:45:49 +0900

    feat: RefreshToken entity, repository 설정

--- backend/src/main/java/com/backend/domain/user/entity/RefreshToken.java
+++ backend/src/main/java/com/backend/domain/user/entity/RefreshToken.java
@@ -1,20 +1,27 @@
 package com.backend.domain.user.entity;
 
-import com.backend.global.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Entity
 @Builder
-@RedisHash(value = "refreshToken", timeToLive = 604800) //7일
-public class RefreshToken extends BaseEntity {
+@RedisHash(value = "refreshToken") //RedisTemplate에서는 필수는 아니지만 일단 사용
+public class RefreshToken {
+
+    @Id
+    private String userId;
 
     private String refreshToken;
+
+    @TimeToLive
+    private Long expiration;
 }
