ad9d64c8d5c735e9886e95eee5ff362b5450dc14
larama-C <ccbj0825@naver.com>
2025-11-10 17:30:04 +0900

    feat: ElasticsearchConfig 설정

---
+++ backend/src/main/java/com/backend/global/config/ElasticsearchConfig.java
@@ -0,0 +1,22 @@
+package com.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
+@Configuration
+public class ElasticsearchConfig extends ElasticsearchConfiguration {
+
+    @Value("${spring.elasticsearch.uris}")
+    private String elasticsearchUrl;
+
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                // URI에서 "http://"를 제거하지 마세요
+                // Elasticsearch가 실제 포트를 포함한 주소를 필요로 합니다.
+                .connectedTo(elasticsearchUrl.replace("http://", "")) // localhost:9200 형태로 변환
+                .build();
+    }
+}
