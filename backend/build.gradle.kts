e9fea173daac0a12918739f41b58b34ed20392be
myoungjinseo <sih5673@gmail.com>
2025-11-12 11:09:54 +0900

    feat: 디스코드 웹훅 #243

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.6"
@@ -22,6 +24,7 @@ configurations {
 
 repositories {
 	mavenCentral()
+    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
@@ -39,15 +42,18 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
     implementation ("org.springframework.boot:spring-boot-starter-mail")
-
+    implementation("com.github.napstr:logback-discord-appender:1.0.0")
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
 	implementation("org.springframework.ai:spring-ai-starter-model-openai")
+    implementation("org.springframework.ai:spring-ai-starter-model-vertex-ai-gemini")
 
 
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 
+    implementation("io.github.resilience4j:resilience4j-spring-boot3:2.3.0")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
 	//querydsl
 	annotationProcessor("io.github.openfeign.querydsl:querydsl-apt:7.1:jpa")
 	implementation("io.github.openfeign.querydsl:querydsl-jpa:7.1")
